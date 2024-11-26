36a07b77fafac88d814507221808fbc6ce19dcd4
DENISA-RALUCA PORUMB <denisa.porumb@stud.ubbcluj.ro>
2024-11-26 22:03:18 +0200

    laborator8

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,24 @@
-﻿namespace PorumbDenisaLab7
+﻿using System;
+using PorumbDenisaLab7.Data;
+using System.IO;
+
+namespace PorumbDenisaLab7
 {
+
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null) 
+                { 
+                    database = new ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "ShoppingList.db3"));
+                } 
+                return database; 
+            } 
+        }
         public App()
         {
             InitializeComponent();
