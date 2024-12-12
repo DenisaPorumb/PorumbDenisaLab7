0b658f0b31f97315d309f079f2b04df2744a423e
DENISA-RALUCA PORUMB <denisa.porumb@stud.ubbcluj.ro>
2024-12-12 11:42:42 +0200

    Laborator10

---
+++ ShopPage.xaml.cs
@@ -0,0 +1,42 @@
+using Microsoft.Maui.Devices.Sensors;
+using Plugin.LocalNotification;
+using PorumbDenisaLab7.Models;
+
+namespace PorumbDenisaLab7;
+public partial class ShopPage : ContentPage
+{
+	public ShopPage()
+	{
+		InitializeComponent();
+	}
+	async void OnSaveButtonClicked(object sender, EventArgs e)
+	{
+		var shop = (Shop)BindingContext;
+		await App.Database.SaveShopAsync(shop);
+		await Navigation.PopAsync();
+	}
+    async void OnShowMapButtonClicked(object sender, EventArgs e) 
+	{
+		var shop = (Shop)BindingContext; 
+		var address = shop.Adress; 
+		/*var locations = await Geocoding.GetLocationsAsync(address); */
+		var options = new MapLaunchOptions { Name = "Magazinul meu preferat" }; 
+	    var shoplocation= new Location(46.7492379, 23.5745597);
+        var myLocation = new Location(46.7731796289, 23.6213886738);
+        var distance = myLocation.CalculateDistance(myLocation, DistanceUnits.Kilometers);
+		if (distance < 5)
+		{
+			var request = new NotificationRequest
+			{
+				Title = "Ai de facut cumparaturi in apropiere!",
+				Description = address,
+				Schedule = new NotificationRequestSchedule
+				{
+					NotifyTime = DateTime.Now.AddSeconds(1)
+				}
+			};
+			LocalNotificationCenter.Current.Show(request);
+		}
+            await Map.OpenAsync(shoplocation, options);
+	}
+}
